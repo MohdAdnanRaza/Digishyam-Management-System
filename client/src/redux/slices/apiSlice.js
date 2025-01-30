247920fe832ed292a05645f619fa7b8bc35b9c3d
Mohd Adnan Raza <130435343+MohdAdnanRaza@users.noreply.github.com>
2025-01-30 12:49:09 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:4000/api";
-
+//const API_URI = "http://localhost:4000/api";
+const API_URI="https://digishyam-management-system-backend.onrender.com";
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
 export const apiSlice = createApi({
