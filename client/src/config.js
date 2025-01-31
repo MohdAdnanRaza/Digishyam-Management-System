bc60361764b98eb7f66a724ed11c4309da33dfd0
Adnan07 <adnankhan0704@gmail.com>
2025-01-31 17:59:05 +0530

    bugs fixes

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
-
+  console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
 export default API_BASE_URL;
