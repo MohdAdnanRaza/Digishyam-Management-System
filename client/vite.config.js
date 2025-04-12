ebc35157a2249769dd46a1a7719a830ba8cea99b
root <root@srv731522.hstgr.cloud>
2025-04-12 06:54:17 +0000

    Your message about changes in vite.config.js and package-lock

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return {
-    define: {
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
-
 
 
